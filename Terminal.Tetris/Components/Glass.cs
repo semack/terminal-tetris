6ffc6ae1e983daf371f02b444710203cbcefcd25
Andriy S'omak <semack@gmail.com>
2020-04-23 18:02:42 +0300

    clean up

--- Terminal.Tetris/Components/Glass.cs
+++ Terminal.Tetris/Components/Glass.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Terminal.Tetris.Common;
@@ -14,15 +16,33 @@ namespace Terminal.Tetris.Components
         private readonly int _x;
         private readonly int _y;
 
+        
+        private static readonly IList<short[,]> _masks = new List<short[,]>(new[]
+        {
+            new short[,] {{1, 1, 1, 1}},
+            new short[,] {{2, 2}, {2, 2}},
+            new short[,] {{0, 0, 3}, {3, 3, 3}},
+            new short[,] {{4, 0, 0}, {4, 4, 4}},
+            new short[,] {{0, 5, 5}, {5, 5, 0}},
+            new short[,] {{6, 6, 0}, {0, 6, 6}},
+            new short[,] {{7, 7, 7}, {0, 7, 0}}
+        });
+
+        private readonly Random _random = new Random();
+
+        public async Task<Block> GeNextBlockAsync(CancellationToken cancellationToken)
+        {
+            var index = _random.Next(_masks.Count - 1);
+            var result = new Block(_masks[index]);
+            return await Task.FromResult(result);
+        }
 
         public Glass(TerminalIO io, int x, int y) : base(io)
         {
             _x = x;
             _y = y;
         }
-
-        public Block NextBlock { get; }
-
+        
         private async Task DrawGlassAsync(CancellationToken cancellationToken = default)
         {
             int i;
